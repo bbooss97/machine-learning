9c6d9b07709184629db9c815326a4228991d53ca
bbooss97 <bbooss97@gmail.com>
2022-12-21 19:17:50 +0100

    nn mlp

--- hw2/nn.py
+++ hw2/nn.py
@@ -1,5 +1,21 @@
 import torch
 import torch.nn as nn
 
-class Net(nn.Module):
+class Mlp(nn.Module):
+    def __init__(self):
+        super(Mlp, self).__init__()
+        self.fc1 = nn.Linear(784, 128)
+        self.fc2 = nn.Linear(128, 128)
+        self.fc3 = nn.Linear(128, 19)
+
+    def forward(self, x):
+        x = self.fc1(x)
+        x=nn.ReLU(x)
+        x = self.fc2(x)
+        x=nn.ReLU(x)
+        x=self.fc3(x)
+        
+        #do not need to use softmax or sigmoid because we use cross entropy loss and it does it for us
+        
+        return x
     
--- hw2/train.py
+++ hw2/train.py
@@ -2,6 +2,10 @@ import torch
 from dataset import OnePieceDataset
 from torch.utils.data import DataLoader
 
+#declare parameters
+num_epochs=10
+batch_size=32
+
 #read the dataset
 dataset=OnePieceDataset()
 
@@ -10,7 +14,9 @@ split=[int(0.8*len(dataset)),int(0.2*len(dataset))+1]
 train,test = torch.utils.data.random_split(dataset,split)
 
 #dataloader
-train_dataloader = DataLoader(train, batch_size=32, shuffle=True)
-test_dataloader = DataLoader(test, batch_size=32, shuffle=True)
+train_dataloader = DataLoader(train, batch_size=batch_size, shuffle=True)
+test_dataloader = DataLoader(test, batch_size=batch_size, shuffle=True)
+
+
 
 print("finished")
